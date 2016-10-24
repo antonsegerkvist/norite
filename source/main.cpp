eae75557c35935d9d3cd2c9fad2ea37f3d1396b4
anton <anton.stefan.roine.segerkvist@gmail.com>
2016-10-24 05:40:44 +0200

    First commit

---
+++ source/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  norite
+//
+//  Created by Anton Segerkvist on 24/10/16.
+//  Copyright © 2016 anton segerkvist. All rights reserved.
+//
+
+#include <cstdlib>
+
+int main(int argc, char** argv)
+{
+  return EXIT_SUCCESS;
+}
